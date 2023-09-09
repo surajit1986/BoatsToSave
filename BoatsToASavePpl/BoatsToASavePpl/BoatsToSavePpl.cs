694616604bc06b44cfbab746d140ee0a2e0c5c0b
Surajit Ghosh <Surajit.1986@gmail.com>
2023-09-09 11:40:27 +0530

    Leetcode problenm to find minimum boats required to carry people with different weights. Boat having weight capacity and carry max 2 ppl

---
+++ BoatsToASavePpl/BoatsToASavePpl/BoatsToSavePpl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoatsToASavePpl
+{
+    internal class BoatsToSavePpl
+    {
+        //This problem will work if no constarint given that a boat can carry max 2 ppl.
+        public int MinBoatCountToSavePpl(int[] weigths, int capacity)
+        {
+            int count = 0;
+            int totalWeight = 0;
+
+            while (count < weigths.Length)
+            {
+                totalWeight += weigths[count];
+            }
+
+            if (totalWeight == 0)
+            {
+                return 0;
+            }
+            else if (totalWeight < capacity)
+            {
+                return 1;
+            }
+            else
+            {
+                count = (totalWeight % capacity);
+                if (count == 0)
+                {
+                    return (totalWeight / capacity);
+                }
+                else
+                {
+                    int remain = totalWeight / capacity;
+                    return (remain + count);
+                }
+            }
+
+        }
+
+        public int MinBoatsWithCapacity(int[] weigths, int capacity)
+        {
+            Array.Sort(weigths);
+
+            int lightW = 0;
+            int heavyW = weigths.Length -1;
+            int boats = 0;
+
+            while (heavyW >= lightW)
+            {
+                if ((weigths[heavyW]+ weigths[lightW])<= capacity) { 
+                   
+                    heavyW--;
+                    lightW++;
+                }
+                else
+                {
+                    heavyW--;
+                    
+                }
+                boats++;
+            }
+
+            return boats;
+        }
+    }
+}
---
+++ BoatsToASavePpl/BoatsToASavePpl/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BoatsToASavePpl;
+
+Console.WriteLine("minimum Boats required to carry no.of people. Boat having a carrying capacity and sitting capacity.");
+
+BoatsToSavePpl boats = new BoatsToSavePpl();
+int count = boats.MinBoatsWithCapacity(new int[] { 3,2,2,1}, 3);
+
+Console.WriteLine($"No. Of boats required: {count}");
+Console.ReadKey();
